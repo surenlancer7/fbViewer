86b9927e310183fd9b3490af1c57d14e3acc0207
Surendher <surendher@Rajus-MacBook-Pro.local>
2016-07-21 13:41:16 +0530

    Minor changes

---
+++ example/fb-controller.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+    var module = angular.module('fbController',['common.directives.formBuilder']);
+    
+    module.controller('fbController', function($scope) {
+    $scope.html = [{"id":"textbox","component":"textInput","editable":false,"index":0,"label":"Name","description":"Your name","placeholder":"Your name","options":[],"required":true,"validation":"/.*/"}];    
+    $scope.goToFormViewer = function(input) {
+        console.log(input);
+        $scope.html = [];
+        $scope.html = input;
+      };
+
+    });
+})();
